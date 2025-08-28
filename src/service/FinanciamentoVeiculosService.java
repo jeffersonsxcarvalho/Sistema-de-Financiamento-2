d246bb1e60fb25034f2601853a45da01a8b95e70
Jefferson S X Carvalho <105809116+jeffersonsxcarvalho@users.noreply.github.com>
2025-08-27 22:12:21 -0300

    Update FinanciamentoVeiculosService.java
    Adicionada a variável de status

--- src/service/FinanciamentoVeiculosService.java
+++ src/service/FinanciamentoVeiculosService.java
@@ -1,70 +1,39 @@
-package service;
+package model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+public class FinanciamentoDeVeiculos extends Financiamento{
 
-import model.Cliente;
-import model.Financiamento;
-import model.FinanciamentoDeVeiculos;
-
-public class FinanciamentoVeiculosService {
-	
-	private List<FinanciamentoDeVeiculos> listVeiculos;
-	
-	public FinanciamentoVeiculosService() {
-		listVeiculos = new ArrayList<FinanciamentoDeVeiculos>();
-	}
-	
-	public FinanciamentoDeVeiculos solicitarDadosFinanciamentoVeiculos(Cliente cliente, Scanner sc) {
-		
-		System.out.println("Digite o valor do veículo: ");
-		double valorDoBem = sc.nextDouble();
-		
-		System.out.println("Digite o número de parcelas: ");
-		int numeroDeParcelas = sc.nextInt();
-
-		System.out.println("Digite o valor da entrada: ");
-		double entrada = sc.nextDouble();
-		
-		FinanciamentoDeVeiculos financiamentoVeiculos = new FinanciamentoDeVeiculos(cliente, valorDoBem, numeroDeParcelas, entrada);
-		
-		try{
-			validarVeiculo(cliente, financiamentoVeiculos);
-	        listVeiculos.add(financiamentoVeiculos);
-	        System.out.println(financiamentoVeiculos.toString());
-	    } catch (IllegalArgumentException e) {
-			System.out.println("Financiamento recusado! " + e.getMessage());
-		}
-	    return financiamentoVeiculos;
-	}
-	
-	private void validarVeiculo(Cliente cliente, Financiamento financiamentoVeiculos) {
-		
-        if(cliente.getIdade() < 18) {
-        	throw new IllegalArgumentException("Idade inválida. É necessário ter no mínimo 18 anos.");
-        } 
-        
-        double entradaMinima = ((FinanciamentoDeVeiculos) financiamentoVeiculos).calcularEntradaMinima();
-        if(financiamentoVeiculos.getEntrada() < entradaMinima) {
-            throw new IllegalArgumentException(String.format(
-                "Entrada insuficiente. A entrada mínima exigida é R$ %.2f.", entradaMinima));
-        }
-        
-        double valorParcela = financiamentoVeiculos.calcularValorParcela();
-        double limite = cliente.getRendaMensal() * 0.20;
-        if(valorParcela > limite) {
-        	throw new IllegalArgumentException(String.format("Renda insuficiente. A parcela (R$ %.2f) excede 20%% da renda mensal (R$ %.2f).\n", valorParcela, limite));
-        }
-        
-        if(financiamentoVeiculos.getNumeroDeParcelas() > 60) {
-        	throw new IllegalArgumentException("Número de parcelas excede o limite máximo de 60 meses.");
-        }
-        
+    public FinanciamentoDeVeiculos(Cliente cliente, double valorDoBem, int numeroDeParcelas, double entrada) {
+        super(cliente, valorDoBem, numeroDeParcelas, entrada);
     }
-	
-	public List<FinanciamentoDeVeiculos> getListVeiculos(){
-    	return listVeiculos;
+
+    public double calcularValorDoFinanciamento() {
+        return getValorDoBem() - getEntrada();
     }
+
+    public double calcularEntradaMinima() {
+        return getValorDoBem() * 0.10;
+    }
+
+    @Override
+    public double calcularValorParcela() {
+        double taxaJuros = 0.0115;
+        double fator = 1 - Math.pow(1 + taxaJuros, -numeroDeParcelas);
+        return calcularValorDoFinanciamento() * taxaJuros / fator;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Financiamento aprovado!\nEntrada miníma: R$ %.2f\n"
+                        + "Valor Financiado: R$ %.2f\nNúmero de parcelas: %d\n"
+                        + "Valor da parcela: R$ %.2f", calcularEntradaMinima(), calcularValorDoFinanciamento(),
+                numeroDeParcelas, calcularValorParcela());
+    }
+
+    public String toStringListagem() {
+        return String.format("Status: %s | Nome: %s | Idade: %d | Renda Mensal: R$ %.2f | Valor Financiamento: R$ %.2f | Número de parcelas: %d | Entrada: R$ %.2f | Valor da parcela: R$ %.2f\n",
+                this.status, cliente.getNome(), cliente.getIdade(), cliente.getRendaMensal(), calcularValorDoFinanciamento(), getNumeroDeParcelas(), getEntrada(), calcularValorParcela());
+    }
+
 
 }
